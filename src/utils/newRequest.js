c995b3d7dcee70d055f67e86f385ea9c9aac362f
JRNJ2 <jerinjosephcherukara@gmail.com>
2024-09-01 17:11:02 +0530

    push

---
+++ src/utils/newRequest.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const newRequest = axios.create({
+    baseURL :"https://renderfamilybackend-c23e.onrender.com",
+    withCredentials:true,
+})
+
+
+export default newRequest
