59fe862e350467473571165661d20ce8db82bf80
JRNJ2 <jerinjosephcherukara@gmail.com>
2024-09-01 18:19:53 +0530

    nnn

--- api/src/main/java/com/familytree/application/config/WebMvcConfig.java
+++ api/src/main/java/com/familytree/application/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","https://renderfamilyfrontend.vercel.app")
+                .allowedOrigins("http://localhost:3000","https://renderfamilyfrontend.vercel.app","https://testpegtree.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
